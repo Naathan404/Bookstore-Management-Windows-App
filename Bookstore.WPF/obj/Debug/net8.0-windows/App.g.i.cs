d89742f6ef16e6fd5c15e7f4cb0d0ad50875527e
nguyenhun11 <giahungcantho11@gmail.com>
2026-03-16 16:46:17 +0700

    add .gitignore

--- Bookstore.WPF/obj/Debug/net8.0-windows/App.g.i.cs
+++ Bookstore.WPF/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Bookstore.WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.4.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Bookstore.WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.4.0")]
         public static void Main() {
             Bookstore.WPF.App app = new Bookstore.WPF.App();
             app.InitializeComponent();
