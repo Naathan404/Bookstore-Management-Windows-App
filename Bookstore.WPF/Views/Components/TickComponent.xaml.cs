ad9287b1b36d66342fefb66d7b8eca543e7e447d
nguyenhun11 <giahungcantho11@gmail.com>
2026-06-02 09:45:21 +0700

    chore: tick component

---
+++ Bookstore.WPF/Views/Components/TickComponent.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bookstore.WPF.Views.Components
+{
+    /// <summary>
+    /// Interaction logic for TickComponent.xaml
+    /// </summary>
+    public partial class TickComponent : UserControl
+    {
+        public TickComponent()
+        {
+            InitializeComponent();
+        }
+
+        // 1. THUỘC TÍNH: IsChecked (Có thể binding TwoWay)
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register("IsChecked", typeof(bool), typeof(TickComponent),
+                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+
+        // 2. THUỘC TÍNH: Command (Hành động khi click)
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(TickComponent),
+                new PropertyMetadata(null));
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        // 3. THUỘC TÍNH: CommandParameter (Dữ liệu truyền theo Command)
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(object), typeof(TickComponent),
+                new PropertyMetadata(null));
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        // THUỘC TÍNH: Text (Để hiển thị chữ bên cạnh ô tick)
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TickComponent),
+                new PropertyMetadata(null)); // Mặc định là null (không có chữ)
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+    }
+}
